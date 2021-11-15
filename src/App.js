a7f73bccec9429edd8a36852298b97fddf87d0b3
Aleksey <alexandro361@gmail.com>
2021-11-15 22:13:51 +0300

    add css style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, {useState} from "react";
 import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
+import './Styles/App.css';
 
 function App() {
-    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('text in input')
 
 
-
-  return (
-    <div className="App">
-        <ClassCounter/>
-        <ClassCounter/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="post">
+                <div className="post__content">
+                    <strong>1. JavaScript</strong>
+                    <div>
+                        JavaScript-язык программирования
+                    </div>
+                </div>
+                    <div className="post_btn">
+                        <button>Удалить</button>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
