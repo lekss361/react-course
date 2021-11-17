4edacd938972edee73cc492a50bc146969e2a2a3
Aleksey <alexandro361@gmail.com>
2021-11-17 17:59:14 +0300

    add props Postitem

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
 import './Styles/App.css';
+import PostItem from "./Components/PostItem";
 
 function App() {
     const [value, setValue] = useState('text in input')
@@ -9,17 +10,7 @@ function App() {
 
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. JavaScript</strong>
-                    <div>
-                        JavaScript-язык программирования
-                    </div>
-                </div>
-                    <div className="post_btn">
-                        <button>Удалить</button>
-                </div>
-            </div>
+        <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
         </div>
     );
 }
